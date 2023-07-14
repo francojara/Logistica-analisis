7724d2ad91ae94c57a1f167cdfe52ea8004c03d1
francojara <101563434+francojara@users.noreply.github.com>
2023-07-14 19:48:51 -0300

    Subiendo pagina
    Se sube la pagina del proyecto

---
+++ logistica(pag)/agregar.js
@@ -0,0 +1,8 @@
+<script>
+  $(document).ready(function() {
+    $('#agregar-elemento').click(function() {
+      // Aquí puedes agregar tu lógica para agregar elementos
+      console.log('Se hizo clic en el botón agregar elemento');
+    })
+  });
+</script>
